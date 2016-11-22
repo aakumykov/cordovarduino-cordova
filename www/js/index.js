9ae817ec12fd0c0669b3e6171b60cb5880dbe19f
Андрей Кумыков <aakumykov@yandex.ru>
2016-11-22 17:39:11 +0700

    Работает пересылка случайных чисел.

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,76 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        var pot = document.getElementById('pot');
+        var delta = document.getElementById('delta');
+        var on = document.getElementById('on');
+        var off = document.getElementById('off');
+        var open = false;
+        var str = '';
+        var lastRead = new Date();
+ 
+        var errorCallback = function(message) {
+            alert('Error: ' + message);
+        };
+        // request permission first 
+        serial.requestPermission(
+            // if user grants permission 
+            function(successMessage) {
+                // open serial port 
+                serial.open(
+                    {baudRate: 9600},
+                    // if port is succesfuly opened 
+                    function(successMessage) {
+                        open = true;
+                        // register the read callback 
+                        serial.registerReadCallback(
+                            function success(data){
+                                // decode the received message 
+                                var view = new Uint8Array(data);
+                                if(view.length >= 1) {
+                                    for(var i=0; i < view.length; i++) {
+                                        // if we received a \n, the message is complete, display it 
+                                        if(view[i] == 13) {
+                                            // check if the read rate correspond to the arduino serial print rate 
+                                            var now = new Date();
+                                            delta.innerText = now - lastRead;
+                                            lastRead = now;
+                                            // display the message 
+                                            var value = parseInt(str);
+                                            pot.innerText = value;
+                                            str = '';
+                                        }
+                                        // if not, concatenate with the begening of the message 
+                                        else {
+                                            var temp_str = String.fromCharCode(view[i]);
+                                            var str_esc = escape(temp_str);
+                                            str += unescape(str_esc);
+                                        }
+                                    }
+                                }
+                            },
+                            // error attaching the callback 
+                            errorCallback
+                        );
+                    },
+                    // error opening the port 
+                    errorCallback
+                );
+            },
+            // user does not grant permission 
+            errorCallback
+        );
+ 
+        on.onclick = function() {
+            console.log('click');
+            if (open) serial.write('1');
+        };
+        off.onclick = function() {
+            if (open) serial.write('0');
+        }
     }
 };
-
+ 
 app.initialize();
